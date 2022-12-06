d29b453f8260c98aa16610c8dc6656951fd8b752
Payal Bannagare <payalbannagare585@gmail.com>
2022-12-06 12:18:09 +0530

    static keyword use

---
+++ OopsPrograms/EmployeeInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPrograms
+{
+    public class EmployeeInfo
+    {
+        private int empid;
+        private string empname, country;
+        private int salary;
+        private static int count;
+        public EmployeeInfo(string empname,int salary, string country = "India")
+        {
+            count++;
+            empid = count;
+            this.empname = empname;
+            this.salary = salary;
+            this.country = country;
+        }
+
+        public override string ToString()
+        {
+            return $"{empid} -> {empname} -> {salary}->{country}";
+        }
+    }
+
+}
+
--- OopsPrograms/StartPoint.cs
+++ OopsPrograms/StartPoint.cs
@@ -10,26 +10,38 @@ namespace OopsPrograms
     {
         public static void Main(string[] args)
         {
-            BookInfo b = new BookInfo();
-            b.BookId = 101;
-            b.BookName = "Revoluation 2020";
-            b.Author = "Chetan Bhagat";
-            b.Price = 1500;
-            b.DisplayBookInfo();
+            //BookInfo b = new BookInfo();
+            //b.BookId = 101;
+            //b.BookName = "Revoluation 2020";
+            //b.Author = "Chetan Bhagat";
+            //b.Price = 1500;
+            //b.DisplayBookInfo();
 
 
-            Console.WriteLine("---------------------------------------");
+            //Console.WriteLine("---------------------------------------");
 
 
-            b.BookId = 103;
-            b.BookName = "Wings of Fire";
-            b.Author = "Abdul Kalam";
-            b.Price = 2000;
-            b.DisplayBookInfo();
+            //b.BookId = 103;
+            //b.BookName = "Wings of Fire";
+            //b.Author = "Abdul Kalam";
+            //b.Price = 2000;
+            //b.DisplayBookInfo();
 
 
-            StudentResultInfo s = new StudentResultInfo("Payal Bannagre", 1041, 75, 78, 80, 79, 75);
-            s.CalculatePercentage();
+            //StudentResultInfo s = new StudentResultInfo("Payal Bannagre", 1041, 75, 78, 80, 79, 75);
+            //s.CalculatePercentage();
+
+
+            EmployeeInfo e1 = new EmployeeInfo("neha tiwari",45000);
+            EmployeeInfo e2 = new EmployeeInfo("komal shah",40000); 
+
+            // named argument
+            EmployeeInfo e3 = new EmployeeInfo("vaishnavi ingle",30000);
+
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
+            Console.WriteLine(e3);
+
 
         }
     }
