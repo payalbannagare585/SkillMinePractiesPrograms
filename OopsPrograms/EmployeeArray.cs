26022280fe08d0974d22af4c5b2ea5d1a068c5c4
Payal Bannagare <payalbannagare585@gmail.com>
2022-12-12 12:39:47 +0530

    Array

---
+++ OopsPrograms/EmployeeArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPrograms
+{
+    public class EmployeeArray
+    {
+        public int Id { get; set; }
+
+        public string Name{ get; set; }
+        public double Salary{ get; set; }
+
+        public static void Main(string[] args)
+        { 
+
+            EmployeeArray[] empArray= new EmployeeArray[]
+                {
+                   new EmployeeArray{Id=101,Name="Payal",Salary=25000},
+                   new EmployeeArray{Id=102,Name="Kalyani",Salary=30000},
+                   new EmployeeArray{Id=103,Name="chanchal",Salary=35000},
+                   new EmployeeArray{Id=104,Name="vaishnavi",Salary=21000},
+                };
+
+            foreach(EmployeeArray item in empArray)
+            {
+                Console.WriteLine($"{item.Id} {item.Name} {item.Salary}");
+            }
+        }
+
+    }
+}
---
+++ OopsPrograms/StudentArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPrograms
+{
+    public class StudentArray
+    {
+         public int id;
+         public string name;
+         public StudentArray(int id,string name)
+        {
+            this.id=id;
+            this.name = name;
+        }
+
+        public static void Main(string[] args)
+        {
+            StudentArray[] studentArrays = new StudentArray[]
+           {
+
+                new StudentArray(123, "vaishnavi"),
+                new StudentArray(135,"Payal"),
+                new StudentArray(145,"kajal")
+                  
+
+            };
+
+            foreach(StudentArray studentArray in studentArrays) 
+            {
+                Console.WriteLine("Student id: "+studentArray.id+"  " +"Student Name: " +studentArray.name);
+               
+            }
+        }
+    }
+}
