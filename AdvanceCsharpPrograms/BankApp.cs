b601bbff6643347708d2830ec84f4a89f4402faa
Payal Bannagare <payalbannagare585@gmail.com>
2022-12-22 11:56:11 +0530

    Delegate demo

--- AdvanceCsharpPrograms/BankApp.cs
+++ AdvanceCsharpPrograms/BankApp.cs
@@ -1,74 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace AdvanceCsharpPrograms
-{
-    internal class BankApp
-    {
+//namespace AdvanceCsharpPrograms
+//{
+//    internal class BankApp
+//    {
 
-        public class Customers
-        {
-            public int CustAccNo { get; set; }
-            public string CustName { get; set; }
+//        public class Customers
+//        {
+//            public int CustAccNo { get; set; }
+//            public string CustName { get; set; }
 
-            public string LoanType { get; set; }
-        }
-        public class AccountType
-        {
-            public string AccoType { get; set; }
+//            //public string LoanType { get; set; }
+
+//            public List<LoanType> loans = new List<LoanType>();
+//        }
+//        public class AccountType
+//        {
+//            public string AccoType { get; set; }
            
 
 
-            public List<Customers> customer = new List<Customers>();
-        }
-
-        static void Main(string[] args)
-        {
-            List<AccountType> accounttypeList = new List<AccountType>()
-            {
-                new AccountType
-                {
-                    AccoType="Saving",
+//            public List<Customers> customer = new List<Customers>();
+//        }
+//        public class LoanType
+//        {
+//            public string Loans { get; set; }
+//        }
+   
+//        static void Main(string[] args)
+//        {
+//            List<AccountType> accounttypeList = new List<AccountType>()
+//            {
+//                new AccountType
+//                {
+//                    AccoType="Saving",
                     
-                    customer={
-                        new Customers{CustAccNo=1012,CustName="User1",LoanType="Home Loan"},
-                        new Customers{CustAccNo=2518,CustName="User2",LoanType="Goald Loan"},
-                        new Customers{CustAccNo=8524,CustName="User10", LoanType = "Home Loan"}
-                    }
-                },
-                new AccountType
-                {
-                    AccoType="Current",
+//                    customer={
+//                        new Customers{CustAccNo=1012,CustName="User1",LoanType="Home Loan"},
+//                        new Customers{CustAccNo=2518,CustName="User2",LoanType="Goald Loan"},
+//                        new Customers{CustAccNo=8524,CustName="User10", LoanType = "Home Loan"}
+//                    }
+//                },
+//                new AccountType
+//                {
+//                    AccoType="Current",
                     
-                    customer={
-                        new Customers{CustAccNo=3155,CustName="User3",LoanType="Personal Loan"},
-                        new Customers{CustAccNo=4455,CustName="User4",LoanType="Home Loan"},
-                        new Customers{CustAccNo=5964,CustName="User5",LoanType = "Home Loan"},
-                    }
-                },
+//                    customer={
+//                        new Customers{CustAccNo=3155,CustName="User3",
+                        
+//                        loans={
 
-            };
+//                            new LoanType{Loans="Home Loan"},
+//                            new LoanType{Loans="Car Loan"},
 
-            foreach (AccountType at in accounttypeList)
-            {
-                Console.WriteLine(at.AccoType);
-                Console.WriteLine("-------------------------------------------");
+//                        },
+
+//                    //         new Customers{CustAccNo=4455,CustName="User4",
+//                    //    loans={
+
+//                    //        new LoanType{Loans="Personal Loan"},
+//                    //        new LoanType{Loans="car Loan"},
+//                    //    },
+//                    //    new Customers{CustAccNo=5964,CustName="User5",
+//                    //    loans={
+//                    //            new LoanType={Loans="Education Loan"},
+//                       },
+//                    }
+//                },
+
+//            };
+
+//            foreach (AccountType at in accounttypeList)
+//            {
+//                Console.WriteLine(at.AccoType);
+//                Console.WriteLine("-------------------------------------------");
             
-                foreach (Customers c in at.customer)
-                {
-                    Console.WriteLine(" "+"Account No: "+ c.CustAccNo+"     "+"Customer Name: " + c.CustName);
-                    Console.WriteLine(" " + "Loan Type: " + c.LoanType);
+//                foreach (Customers c in at.customer)
+//                {
+//                    Console.WriteLine(" "+"Account No: "+ c.CustAccNo+"     "+"Customer Name: " + c.CustName);
+//                    foreach(LoanType l in c.loans)
+//                    {
+//                        Console.WriteLine(l.Loans);
+//                    }
                     
-                }
-            }
+//                }
+//            }
 
 
-        }
-    } 
+//        }
+//    } 
 
-} 
+//} 
 
         
---
+++ AdvanceCsharpPrograms/DelegatesAndEvent Programs/DelegateDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharpPrograms.DelegatesAndEvent_Programs
+{
+    public class DelegateDemo
+    {
+        public delegate int Mydel1(int n1, int n2);
+        public delegate string Mydel2(string s);
+
+        public class Test
+        {
+            public int M1(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int M3(int a, int b)
+            {
+                return a * b;
+            }
+
+            public int M4(int a, int b)
+            {
+                return a - b;
+            }
+            public string M2(string name)
+            {
+                return name.ToUpper();
+            }
+
+
+            public static void Main(string[] args)
+            {
+
+                Test t1 = new Test();
+                Mydel1 d1 = new Mydel1(t1.M1);//added the method referance
+
+                d1 += new Mydel1(t1.M1);
+                d1 += new Mydel1(t1.M4);
+
+                Delegate[] list = d1.GetInvocationList();
+
+                foreach(Delegate items in list)
+                {
+                    Console.WriteLine(items.Method);
+                    Console.WriteLine(items.DynamicInvoke(76, 32));
+                }
+
+                Mydel2 d2 = new Mydel2(t1.M2);
+
+                string str = d2.Invoke("THINK QUEOTIENT");
+
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
